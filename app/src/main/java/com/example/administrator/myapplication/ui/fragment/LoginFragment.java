8f4969ccc1f36341d6e4468f90838c62f496869c
squirrel1021 <ouchenyin@163.com>
2017-05-06 00:23:50 +0800

    添加启动页

--- app/src/main/java/com/example/administrator/myapplication/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/ui/fragment/LoginFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.administrator.myapplication.R;
@@ -18,30 +19,13 @@ import com.example.administrator.myapplication.R;
  * 登录
  */
 public class LoginFragment extends Fragment {
-
+private EditText username,password;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_login, null);
-        TextView tv_down = (TextView)view.findViewById(R.id.tv_down);
-        tv_down.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                gotoRankMe();
-            }
-        });
+       username= (EditText) view.findViewById(R.id.et_phone);
+        password= (EditText) view.findViewById(R.id.et_psw);
         return view;
     }
 
-    /**
-     * 下载正式版小红书
-     */
-    private void gotoRankMe() {
-        try {
-            Uri uri = Uri.parse("market://details?id=com.xingin.xhs");
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-        }
-    }
 }
