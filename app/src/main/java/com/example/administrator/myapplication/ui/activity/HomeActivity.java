2cdc21b817315367bdfaddc59fa83e1def1ad52a
squirrel1021 <ouchenyin@163.com>
2017-05-18 23:49:09 +0800

    增加分享和第三方登录（暂支持qq）

--- app/src/main/java/com/example/administrator/myapplication/ui/activity/HomeActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/ui/activity/HomeActivity.java
@@ -6,7 +6,11 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+
+import com.bumptech.glide.Glide;
 import com.example.administrator.myapplication.R;
 import com.example.administrator.myapplication.utils.ShareUtil;
 
@@ -25,6 +29,13 @@ public class HomeActivity extends Activity {
     }
 
     private void initView() {
+        String nickname=getIntent().getStringExtra("nickname");
+        String imageUrl=getIntent().getStringExtra("headImage");
+        ((TextView)findViewById(R.id.welcomeTv)).setText("欢迎"+nickname+"");
+        ImageView headImage= (ImageView) findViewById(R.id.headImage);
+        Glide.with(this)
+                .load(imageUrl)
+                .into(headImage);
         shareBtn = (Button) findViewById(R.id.shareBtn);
         shareBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/administrator/myapplication/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/ui/fragment/LoginFragment.java
@@ -1,10 +1,12 @@
 package com.example.administrator.myapplication.ui.fragment;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,20 +24,30 @@ import com.example.administrator.myapplication.bean.UserTable;
 import com.example.administrator.myapplication.bean._User;
 import com.example.administrator.myapplication.ui.activity.HomeActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.tencent.qq.QQ;
 
 
 /**
  * 登录
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener{
     private EditText username, password;
     private View view;
     private Button button;
+    private ImageView qqLogin,wxLogin,weiboLogin;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -47,6 +60,12 @@ public class LoginFragment extends Fragment {
         username = (EditText) view.findViewById(R.id.et_phone);
         password = (EditText) view.findViewById(R.id.et_psw);
         button= (Button) view.findViewById(R.id.login_btn);
+        qqLogin= (ImageView) view.findViewById(R.id.iv_qq);
+        wxLogin= (ImageView) view.findViewById(R.id.iv_wechat);
+        weiboLogin= (ImageView) view.findViewById(R.id.iv_weibo);
+        qqLogin.setOnClickListener(this);
+        wxLogin.setOnClickListener(this);
+        weiboLogin.setOnClickListener(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,4 +101,75 @@ public class LoginFragment extends Fragment {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            //qq登陆
+            case R.id.iv_qq:
+            loginByQQ();
+                break;
+            //微信登陆
+            case R.id.iv_wechat:
+
+                break;
+            //微博登陆
+            case R.id.iv_weibo:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loginByQQ() {
+        Platform qq = ShareSDK.getPlatform(QQ.NAME);
+//回调信息，可以在这里获取基本的授权返回的信息，但是注意如果做提示和UI操作要传到主线程handler里去执行
+        qq.setPlatformActionListener(new PlatformActionListener() {
+
+            @Override
+            public void onError(Platform arg0, int arg1, Throwable arg2) {
+                // TODO Auto-generated method stub
+                arg2.printStackTrace();
+            }
+
+            @Override
+            public void onComplete(final Platform arg0, int arg1, HashMap<String, Object> arg2) {
+                // TODO Auto-generated method stub
+                //输出所有授权信息
+                arg0.getDb().exportData();
+                try {
+                     JSONObject json=new JSONObject(arg0.getDb().exportData());
+                    final String nickname=json.getString("nickname")+"";
+                    final  String icon=json.getString("icon")+"";
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Intent intent=new Intent();
+                            intent.putExtra("nickname",nickname);
+                            intent.putExtra("headImage",icon);
+                            intent.setClass(getActivity(), HomeActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onCancel(Platform arg0, int arg1) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+//authorize与showUser单独调用一个即可
+//        QQ.authorize();//单独授权,OnComplete返回的hashmap是空的
+        qq.showUser(null);//授权并获取用户信息
+//移除授权
+//weibo.removeAccount(true);
+    }
 }
